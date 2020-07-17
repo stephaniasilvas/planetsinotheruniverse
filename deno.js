1fa1452efe1a35a1336688d7d7a770f2d6a7bcd8
Stephania <you@example.com>
2020-07-17 18:00:13 +0100

    add .

---
+++ deno.js
@@ -0,0 +1,10 @@
+const food = Deno.args[0];
+const parents = Deno.args[1] ;
+
+
+
+if (food=== "love" && parents === "Mom"){
+    console.log("this🧛‍♀️ born");
+}else{
+    console.log("The baby is not born");
+}
